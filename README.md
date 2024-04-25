a6647042c3146f512333a745e5b71d7124621df5
azamatyashinov <azamatyashinovv@gmail.com>
2024-04-24 20:05:51 -0700

    ifjog

---
+++ README.md
@@ -0,0 +1,50 @@
+#                Lesson 39
+
+- Number
+    # properties  
+        EPSILON
+        MAX_VALUE
+        MAX_SAFE_INTEGER
+        MIN_SAFE_INTEGER
+        POSITIVE_INFINITY
+        NEGATIVE_INFINITY
+        NAN
+    # methods
+        n.toString()      -> sonni sring ga aylantirish uchun qo'llaniladi
+        n.toExponential() -> Sonni standar shaklga keltiradi
+        n.toFixed()       -> Sonning kasr qismini aytilgan xonagacha standart shaklga keltiradi
+        n.toPrecision()   -> sonni boshidan boshlab aytilgan joygacha yaxlitlaydi
+        parselnt(n)       -> Sonning butun qismini kesib beradi
+        parseFloat(n)     -> sonni 0,1,2,3,4,5,6,7,8,9 va . dan boshqa belgigacha kesib tashlaydi
+        Number.isInteger()-> Butunson yoki butun emasligini tekshiradi
+
+- Type conversion & coercion
+    # string -> number
+        Number()
+        parseFloat(n)
+        parselnt(n) 
+        n*1 ->type coercion
+        +n  ->type coercion
+
+    # number  -> string
+        String()
+        n.toString
+        n+"" ->type coercion
+
+- Boolean
+        == -> son va string bo'lsa ham qiymatlari teng bo'lsa true chiqaradi
+        === -> type ham qiymati ham bir xil bo'lganda true chiqaradi
+        != -> teng emasmi  savoliga true javob olish uchun qo'llaniladi
+        >
+        <
+        >=
+        <=
+
+
+
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,141 @@
+//  Boolean-1
+
+// let A;
+// A = +prompt("A sonni kiriting__")
+// console.log(A%2===0)
+
+// Boolean-2
+
+// let A;
+// let B;
+// A = +prompt("A sonni kiriting__")
+// B = +prompt("B sonni kiriting__")
+// console.log(parseInt(A)===A && B===parseInt(B))
+
+//   Boolean-3
+
+// let A;
+// let B;
+// let C;
+// A = +prompt("A sonini kiriting__")
+// B = +prompt("B sonini kiriting__")
+// C = +prompt("C sonini kiriting__")
+// console.log(A<B<C || A>B>C)
+
+
+// Boolean-4
+
+// let A;
+// let B;
+// A = +prompt("A sonini kiriting__")
+// B = +prompt("B sonini kiriting__")
+// console.log(A%2===1 && B%2===1)
+
+
+// Boolean-5
+
+// let A;
+// let B;
+// A = +prompt("A sonini kiriting__")
+// B = +prompt("B sonini kiriting__")
+// console.log(A%2===1 || B%2===1)
+
+
+// Boolean-6
+
+// let A;
+// let B;
+// A = +prompt("A sonini kiriting__")
+// B = +prompt("B sonini kiriting__")
+// console.log(A%2===1 || B%2===1)
+
+// Boolean-7
+
+// let A;
+// let B;
+// let C;
+// A = +prompt("A sonini kiriting__")
+// B = +prompt("B sonini kiriting__")
+// C = +prompt("C sonini kiriting__")
+// console.log(A>0 && B>0 &&C>0)
+
+// Boolean-8
+
+
+// let A;
+// let B;
+// let C;
+// A = +prompt("A sonini kiriting__")
+// B = +prompt("B sonini kiriting__")
+// C = +prompt("C sonini kiriting__")
+// console.log(A>0 && B<0 && C<0 || A<0 && B>0 && C<0 || A<0 && B<0 && C>0)
+
+// Boolean-9
+
+// let A;
+// let B;
+// let C;
+// A = +prompt("A sonini kiriting__")
+// B = +prompt("B sonini kiriting__")
+// C = +prompt("C sonini kiriting__")
+// console.log(A>0 && B>0 && C<0 || A<0 && B>0 && C>0 || A>0 && B<0 && C>0)
+
+
+// Boolean-10
+
+// let A;
+// A = +prompt("A sonini kiriting__")
+// console.log(A>0 && parseInt(A)===A)
+
+
+// Boolean-11
+
+// let A;
+// A = +prompt("A sonini kiriting__")
+// console.log( A>0 && A%1000===A && A%2===1)
+
+// Boolean-12
+
+// let A;
+// let B;
+// let C;
+// A = +prompt("A sonini kiriting__")
+// B = +prompt("B sonini kiriting__")
+// C = +prompt("C sonini kiriting__")
+// console.log(A + B===0 || A+C==0 || B+C===0)
+
+// Boolean-13
+
+// let A;
+// A = +prompt("A uch xonali sonini kiriting__")
+// birlik = A%10
+// onlik = (A.toPrecision(2)/10)%10
+// yuzlik = (A.toPrecision(1)/100)
+// console.log(birlik >onlik && onlik>yuzlik)
+
+// Boolean-14
+
+// let A;
+// A = +prompt("A uch xonali sonini kiriting__")
+// birlik = A%10
+// onlik = (A.toPrecision(2)/10)%10
+// yuzlik = (A.toPrecision(1)/100)
+// console.log(birlik >onlik && onlik>yuzlik || birlik <onlik && onlik<yuzlik)
+
+
+// Boolean-15
+
+// let A;
+// A = +prompt("A uch xonali sonini kiriting__")
+// birlik = A%10
+// yuzlik = (A.toPrecision(1)/100)
+// console.log(birlik === yuzlik)
+
+
+
+
+
+
+
+
+
